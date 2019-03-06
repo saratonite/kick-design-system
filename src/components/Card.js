7f2f38ecdbc7c5296d4b47b39247c9509a72a1f6
Sarath <sarath.hacks@gmail.com>
2019-03-06 09:51:21 +0530

    Updated card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,19 @@
-import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div `
-  border: solid 1px #cecece;
-  padding:10px;
-  box-shadow: 0 2px 10px rgba(0,0,0,0.12);
-  border-radius:2px;
-
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 5px 0px;
+  margin-bottom: 50px;
+  background: rgb(255, 255, 255);
+  border-radius: ${(props) => props.round ? props.round : '5px'};
+  padding: 10px 30px;
+  transition: all 0.2s ease-in;
+  ${(props) => props.dark && `
+    background-color: #3e3e3e;
+    color: #f3f3f3;
+  `}
+  :hover {
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 5px 2px;
+  }
 `;
 
 export default Card;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -19,4 +19,24 @@ storiesOf("Button")
 
 // Card Story
 
-storiesOf("Card").add("Default Card", () => <Card>Coolest card </Card>);
+storiesOf("Card")
+  .add("Default Card", () => <Card> Coolest card </Card>)
+  .add("Card with buttons", () => (
+    <Card>
+      <h2> Love this </h2>{" "}
+      <p>
+        <Button> 😎Hello </Button> <Button primary> 😀Hello👍 </Button>{" "}
+      </p>{" "}
+    </Card>
+  ))
+  .add("Card with round property", () => (
+    <Card round="10px">
+      <h2> Little more rounded </h2>{" "}
+    </Card>
+  ))
+
+  .add("Dark card", () => (
+    <Card dark>
+      <h2> Dark card </h2>{" "}
+    </Card>
+  ));
